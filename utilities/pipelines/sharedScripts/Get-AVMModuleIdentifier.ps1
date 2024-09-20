a5d6d6324961e1ce365d9e69edb6d7a13ad3c18b
Anders Eide <anders.eide@outlook.com>
2024-09-20 13:00:56 +0200

    Improved documentation

--- utilities/pipelines/sharedScripts/Get-AVMModuleIdentifier.ps1
+++ utilities/pipelines/sharedScripts/Get-AVMModuleIdentifier.ps1
@@ -1,4 +1,20 @@
-﻿function Get-AVMModuleIdentifier {
+﻿<#
+.SYNOPSIS
+This function gets the module identifier for a bicep module.
+
+.DESCRIPTION
+This function idnetifies module identifier from metadata property "moduleIdentifier".
+If the metadata does not exist, it will fallback to use the path to identify the name of the module.
+
+.PARAMETER ModuleFolderPath
+Mandatory. Path to the Bicep/ARM module that is being identified
+
+.EXAMPLE
+
+Get-AVMModuleIdentifier -ModuleFolderPath .\avm\res\storage\storage-account
+
+#>
+function Get-AVMModuleIdentifier {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -12,32 +28,38 @@
 
     process {
 
-        $moduleArmTemplateFilePath = Join-Path $ModuleFolderPath 'main.json'
-        $moduleArmTemplateContent = Get-Content -Path $moduleArmTemplateFilePath -Raw
+        if ($moduleArmTemplateFilePath = Join-Path $ModuleFolderPath 'main.json') {
 
-        $moduleData = $moduleArmTemplateContent | ConvertFrom-Json
+            # Get content of the main.json file.
+            $moduleArmTemplateContent = Get-Content -Path $moduleArmTemplateFilePath -Raw
+            $moduleData = $moduleArmTemplateContent | ConvertFrom-Json
 
-        if ($moduleIdentifier = $moduleData.metadata.moduleIdentifier) {
-            # Return value based on module metadata
-            return $moduleIdentifier
-        } else {
-            # Return Module Identified based in path
-
-            # some/path/modules/res/storage/storage-account -> modules/res/storage/storage-account
-            # some/path/avm/res/storage/storage-account -> avm/res/storage/storage-account
-            # some/path/avm/res/storage/storage-account/blob-service/container -> avm/res/storage/storage-account/blob-service/container
-            # some/path/whatever/ptn/some/thing -> whatever/ptn/some/thing
-
-            $folderPathNormalized = $ModuleFolderPath.Replace('\', '/')
-            $extractPattern = '\b(\w+/(res|ptn|utl).*)'
-
-            if ($folderPathNormalized -match $extractPattern) {
-                $moduleIdentifier = $matches[1]
+            # Try to find the module identifier from metadata. Fall back to path based if non-existing.
+            if ($moduleIdentifier = $moduleData.metadata.moduleIdentifier) {
+                # Return value based on module metadata
                 return $moduleIdentifier
             } else {
-                Write-Error 'Not able to extract name from given path'
+                # Return Module Identified based in path
+
+                # some/path/modules/res/storage/storage-account -> modules/res/storage/storage-account
+                # some/path/avm/res/storage/storage-account -> avm/res/storage/storage-account
+                # some/path/avm/res/storage/storage-account/blob-service/container -> avm/res/storage/storage-account/blob-service/container
+                # some/path/whatever/ptn/some/thing -> whatever/ptn/some/thing
+
+                $folderPathNormalized = $ModuleFolderPath.Replace('\', '/')
+                $extractPattern = '\b(\w+\/(res|ptn|utl).*)(?=\/)'
+
+                if ($folderPathNormalized -match $extractPattern) {
+                    $moduleIdentifier = $matches[1]
+                    return $moduleIdentifier
+                } else {
+                    Write-Error 'Not able to extract name from path. Verify that point to a valid resource, pattern or utility module'
+                }
             }
+        } else {
+            Write-Error "Unable to locate 'main.json' file."
         }
+
     }
 
     end {
